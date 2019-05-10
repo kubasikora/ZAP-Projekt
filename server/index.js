fac59d461ba87d5bc135074d04cebf068f73e0b9
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-05-10 18:14:38 +0200

    post job

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app  = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/data', (req, res) => {
+    var query = req.body.data;
+    console.log(query);
+    res.send("OK");
+});
 
 app.get('/', (req, res) => {
     res.send("Hello world!\n");
--- src/main/user_main.c
+++ src/main/user_main.c
@@ -23,16 +23,17 @@
 // server info
 #define WEB_SERVER "dell"
 #define WEB_PORT "8081"
-#define WEB_URL "/"
+#define GET_WEB_URL "/"
+#define POST_WEB_URL "/data"
 
 
-static const char* GET_REQUEST = "GET " WEB_URL " HTTP/1.0\r\n"
+static const char* GET_REQUEST = "GET " GET_WEB_URL " HTTP/1.0\r\n"
     "Host: "WEB_SERVER":"WEB_PORT"\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
     "\r\n";
 
-static const char* POST_REQUEST = "POST " WEB_URL " HTTP/1.0\r\n"
-    "Host: "WEB_SERVER"\r\n"
+static const char* POST_REQUEST = "POST " POST_WEB_URL " HTTP/1.0\r\n"
+    "Host: "WEB_SERVER":"WEB_PORT"\r\n"
     "User-Agent: esp-idf/1.0 esp32\r\n"
     "\r\n";
 
@@ -75,7 +76,7 @@ static esp_err_t event_handler(void *ctx, system_event_t *event){
 
 
 static void http_get_task(void *pvParameters){
-    static const char *TAG = "HTTP_TASK";
+    static const char *TAG = "HTTP_GET_TASK";
     const struct addrinfo hints = {
         .ai_family = AF_INET,
         .ai_socktype = SOCK_STREAM,
@@ -117,7 +118,7 @@ static void http_get_task(void *pvParameters){
             vTaskDelay(1000 / portTICK_PERIOD_MS);
             continue;
         }
-        ESP_LOGI(TAG, "connected to %s", WEB_URL);
+        ESP_LOGI(TAG, "connected to %s", GET_WEB_URL);
         freeaddrinfo(res);
 
         // Write request to socket
@@ -152,11 +153,94 @@ static void http_get_task(void *pvParameters){
         ESP_LOGI(TAG, "Done reading from socket");
         close(s);
 
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
         ESP_LOGI(TAG, "Starting again!");
     }
 }
 
+
+static void http_post_task(void *pvParameters){
+    static const char *TAG = "HTTP_POST_TASK";
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    while(1){
+        xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
+        ESP_LOGI(TAG, "Connected to AP");
+
+        // find IP of service
+        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+        if (err != 0 || res == NULL){
+            ESP_LOGE(TAG, "DNS lookup failed error = %d, res = %p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        addr = &((struct sockaddr_in*)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeded. IP=%s", inet_ntoa(*addr));
+
+        // Create socket
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if(s < 0){
+            ESP_LOGE(TAG, "Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "Allocated socket");
+
+        // Connect to socket
+        if(connect(s, res->ai_addr, res->ai_addrlen) != 0){
+            ESP_LOGE(TAG, "Failed to connect with socket, errno = %d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "connected to %s", POST_WEB_URL);
+        freeaddrinfo(res);
+
+        // Write request to socket
+        if(write(s, POST_REQUEST, strlen(POST_REQUEST)) < 0){
+            ESP_LOGE(TAG, "Failed to send to socket");
+            close(s);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "Socket send success");
+
+        // Set socket receiving timeout
+        struct timeval receiving_timeout;
+        receiving_timeout.tv_sec = 5;
+        receiving_timeout.tv_usec = 0;
+        if(setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout, sizeof(receiving_timeout)) < 0){
+            ESP_LOGE(TAG, "Failed to set socket receiving output");
+            close(s);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "Set socket receiving timeout success");
+
+        // Read HTTP response 
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++){
+                putchar(recv_buf[i]); //print response
+            }
+        } while(r > 0);
+        ESP_LOGI(TAG, "Done reading from socket");
+        close(s);
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Starting again!");
+    }
+}
 
 
 static void echo_task(){
@@ -213,4 +297,5 @@ void app_main(){
 
     xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
     xTaskCreate(http_get_task, "http_get_task", 4096, NULL, 10, NULL);
+    xTaskCreate(http_post_task, "http_post_task", 4096, NULL, 10, NULL);
 }
